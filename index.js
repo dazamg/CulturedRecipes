7bfc96fd1c49f24ac5e3710ce0fb5a3643a505ec
dazmag <iscope09@hotmail.com>
2020-11-18 19:23:12 -0500

    my heroku app

--- index.js
+++ index.js
@@ -150,6 +150,6 @@ app.get('/deserts', (req, res)=>{
 })
 
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('you\'re listening to the spooky sounds of port 8000')
 })
